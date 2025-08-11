96226f3d4443cd7c4f3639ce8aec7685fe479a8d
cbt <quocchung.than@gmail.com>
2025-08-11 11:32:37 +0700

    fix lint

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { ApiError, toErrorResponse } from '../lib/errors';
 
 // 4 parameters to let express understands this is an errorHandler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
   const status = err instanceof ApiError ? err.statusCode : 500;
   const body = toErrorResponse(err);
